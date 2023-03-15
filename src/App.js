131c74bb69df7b4bf6bfc37ff8b68f6e477e85f0
DiasDia11 <106533792+DiasDia11@users.noreply.github.com>
2023-03-15 12:01:06 +0000

    Criando components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@
 import './App.css';
 
 //React
-import { useCallback, } from 'react';
+import { useCallback, useState, } from 'react';
+
+//data
+import { wordList } from " ./data/words";
+
+// components
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+import { wordsList } from './data/words';
+
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  console.log(words)
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'start' &&  <StartScreen />};
+      {gameStage === 'game' &&  <Game />};
+      {gameStage === 'end' &&  <GameOver />};
     </div>
   );
 }
